7e63d33e09e32ab1999edbf867e1d8c11224bc00
Gowri0504 <gowrid.23it@kongu.edu>
2026-03-26 21:42:42 +0530

    fix: add missing List import in RegisterRequest

--- server/src/main/java/com/tracker/placementtracker/dto/RegisterRequest.java
+++ server/src/main/java/com/tracker/placementtracker/dto/RegisterRequest.java
@@ -1,6 +1,7 @@
 package com.tracker.placementtracker.dto;
 
 import com.tracker.placementtracker.entity.Role;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
